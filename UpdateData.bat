940d84cc0b6f43f684034d745b731d5e8d96e852
Misha <66367898+BlyatBeauty@users.noreply.github.com>
2022-12-02 21:33:07 -0500

    Update UpdateData.bat

--- UpdateData.bat
+++ UpdateData.bat
@@ -1,5 +1,7 @@
+@echo off
+
 powershell -Command "(New-Object Net.WebClient).DownloadFile('http://github.com/PCGen/pcgen/archive/refs/heads/master.zip', 'master.zip')"
-powershell -Command "Expand-Archive -Path master.zip -DestinationPath '%~dp0\'"
+powershell -Command "Expand-Archive -Path master.zip -DestinationPath '%~dp0'"
 echo d | xcopy /Y /E "pcgen-master\data" "%~dp0data"
 del "master.zip"
 RMDIR "pcgen-master" /S /Q
