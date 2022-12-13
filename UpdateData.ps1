00015327c380363ccedb484a26a10a90485affd1
Misha <66367898+BlyatBeauty@users.noreply.github.com>
2022-12-13 10:43:07 -0500

    Update UpdateData.ps1

--- UpdateData.ps1
+++ UpdateData.ps1
@@ -1,9 +1,18 @@
-﻿$Url = "http://github.com/PCGen/pcgen/archive/refs/heads/"
+$Url1 = "http://github.com/PCGen/pcgen/archive/refs/heads/"
+$Url2 = "http://github.com/BahamutDragon/pcgen/archive/refs/heads/"
 $LocalPath = ".\"
 $File = "master.zip"
 $Data = ".\pcgen-master\data"
-Invoke-WebRequest ($Url + $File) -OutFile ($LocalPath + $File)
+$Bahamut = ".\pcgen-master\*"
+Invoke-WebRequest ($Url1 + $File) -OutFile ($LocalPath + $File)
 Expand-Archive -Path ($LocalPath + $File) -DestinationPath $LocalPath
 Copy-Item -Path $Data -Destination $LocalPath -Recurse -PassThru -Force
 Remove-Item -Path ".\pcgen-master" -Recurse
 Remove-Item -Path ($LocalPath + $File) -Force
+Invoke-WebRequest ($Url2 + $File) -OutFile ($LocalPath + $File)
+Expand-Archive -Path ($LocalPath + $File) -DestinationPath $LocalPath
+Copy-Item -Path $Bahamut -Destination $LocalPath -Recurse -PassThru -Force
+Remove-Item -Path ".\pcgen-master" -Recurse
+Remove-Item -Path ($LocalPath + $File) -Force
+Remove-Item -Path $LocalPath .gitattributes -Force
+Remove-Item -Path $LocalPath .gitignore -Force
