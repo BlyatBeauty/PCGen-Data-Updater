b02fd6a7822ee979fcfd083248c0deba3e448427
Misha <66367898+BlyatBeauty@users.noreply.github.com>
2022-12-13 12:14:20 -0500

    Update UpdateData.sh

--- UpdateData.sh
+++ UpdateData.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This version of the script relies on the user applying the variables to the script before running the script - avoiding hardlinks and making the script easier for anyone to run.
+# The script struggles with spaces in the directory names so ensure your paths don't have spaces in your path names.
+# If you're struggling to run the script, the command looks something like this: "bash UpdateData.sh /tmp /home/YourNameHere/pcgen". Remember you're supplying the variables yourself when you run the script.
+
 if [ $# -ne 2 ]; then
         echo "$0: Keep your PCGen data in sync with the repository"
         echo ""
@@ -20,7 +24,15 @@ if [ ! -e "pcgen-master" ]; then
         git clone --depth 1 "https://github.com/PCGen/pcgen.git" "pcgen-master" || { echo "ERROR cloning repository, aborting!"; exit 2; }
 else
         # update / hot pull
-        cd pcgen-master && git pull || { echo "ERROR updating repository, aborting!"; exit 4; }
+        cd pcgen-master && git pull || { echo "ERROR updating repository, aborting!"; exit 3; }
+fi
+if [ ! -e "bahamut-master" ]; then
+        # new / cold  checkout
+        git clone --depth 1 "https://github.com/BahamutDragon/pcgen.git" "bahamut-master" || { echo "ERROR cloning repository, aborting!"; exit 4; }
+else
+        # update / hot pull
+        cd bahamut-master && git pull || { echo "ERROR updating repository, aborting!"; exit 5; }
 fi
 
-rsync -s -av --protect-args --progress "$WORKDIR/pcgen-master/data/" "$DISTDIR/data/" || { echo "ERROR syncing updates, aborting!"; exit 8; }
+rsync -s -av --protect-args --progress "$WORKDIR/pcgen-master/data/" "$DISTDIR/data/" || { echo "ERROR syncing updates, aborting!"; exit 6; }
+rsync -s -av --protect-args --progress "$WORKDIR/bahamut-master/" "$DISTDIR/" || { echo "ERROR syncing updates, aborting!"; exit 7; }
