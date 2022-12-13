998597807e9bd5a96ef411eb289babc9cef5f323
Misha <66367898+BlyatBeauty@users.noreply.github.com>
2022-12-13 10:00:18 -0500

    Update UpdateData.bat
    By popular demand from the PCGen discord, I've added BahamutDragon's sources to the script. When the script adds the most recent data from PCGen's master repo, it will also add of BahamutDragon's sources as well.

--- UpdateData.bat
+++ UpdateData.bat
@@ -1,8 +1,16 @@
-@echo off
-
+REM ===== PCGen main repo =====
 powershell -Command "(New-Object Net.WebClient).DownloadFile('http://github.com/PCGen/pcgen/archive/refs/heads/master.zip', 'master.zip')"
 powershell -Command "Expand-Archive -Path master.zip -DestinationPath '%~dp0'"
 echo d | xcopy /Y /E "pcgen-master\data" "%~dp0data"
 del "master.zip"
+RMDIR "pcgen-master" /S /Q
+REM ===== Bahamut Sources repo =====
+powershell -Command "(New-Object Net.WebClient).DownloadFile('http://github.com/BahamutDragon/pcgen/archive/refs/heads/master.zip', 'bahamut.zip')"
+powershell -Command "Expand-Archive -Path bahamut.zip -DestinationPath '%~dp0'"
+echo d | xcopy /Y /E "pcgen-master\data" "%~dp0data"
+echo d | xcopy /Y /E "pcgen-master\outputsheets" "%~dp0data"
+echo d | xcopy /Y /E "pcgen-master\preview" "%~dp0data"
+echo d | xcopy /Y /E "pcgen-master\system" "%~dp0data"
+del "bahamut.zip"
 RMDIR "pcgen-master" /S /Q
 exit
