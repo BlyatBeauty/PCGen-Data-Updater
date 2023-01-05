64b62d4c3fb54850630a62351e1121b467e2698d
Misha <66367898+BlyatBeauty@users.noreply.github.com>
2023-01-05 15:57:26 -0500

    MicroRepo Update
    This update changes where the Update script is pointing at when looking for new data. Instead of originally using PCGen's master repo + BahamutDragon's PCGen repo, it now points at a new repo I started which takes its contents from PCGen and BahamutDragon automatically on the backend. 
    The end result is a much smaller download, hopefully increasing the speed at which the script runs and decreases the wear on the end-user's SSD/HDD.

--- UpdateData.sh
+++ UpdateData.sh
@@ -2,7 +2,7 @@
 
 # This version of the script relies on the user applying the variables to the script before running the script - avoiding hardlinks and making the script easier for anyone to run.
 # The script struggles with spaces in the directory names so ensure your paths don't have spaces in your path names.
-# If you're struggling to run the script, the command looks something like this: "bash UpdateData.sh /tmp /home/YourNameHere/pcgen". Remember you're supplying the variables yourself when you run the script.
+# If you're struggling to run the script, the command looks something like this: "bash UpdateData.sh /tmp /home/YourNameHere/programs/pcgen". Remember you're supplying the variables yourself when you run the script.
 
 if [ $# -ne 2 ]; then
         echo "$0: Keep your PCGen data in sync with the repository"
@@ -19,20 +19,12 @@ DISTDIR="$2"
 
 cd $WORKDIR || { echo "ERROR entering WORKDIR, aborting!"; exit 1; }
 
-if [ ! -e "pcgen-master" ]; then
+if [ ! -e "blyatbeauty-master" ]; then
         # new / cold  checkout
-        git clone --depth 1 "https://github.com/PCGen/pcgen.git" "pcgen-master" || { echo "ERROR cloning repository, aborting!"; exit 2; }
+        git clone --depth 1 "https://github.com/BlyatBeauty/PCGen-Data.git" "blyatbeauty-master" || { echo "ERROR cloning repository, aborting!"; exit 2; }
 else
         # update / hot pull
-        cd pcgen-master && git pull || { echo "ERROR updating repository, aborting!"; exit 3; }
-fi
-if [ ! -e "bahamut-master" ]; then
-        # new / cold  checkout
-        git clone --depth 1 "https://github.com/BahamutDragon/pcgen.git" "bahamut-master" || { echo "ERROR cloning repository, aborting!"; exit 4; }
-else
-        # update / hot pull
-        cd bahamut-master && git pull || { echo "ERROR updating repository, aborting!"; exit 5; }
+        cd blyatbeauty-master && git pull || { echo "ERROR updating repository, aborting!"; exit 3; }
 fi
 
-rsync -s -av --protect-args --progress "$WORKDIR/pcgen-master/data/" "$DISTDIR/data/" || { echo "ERROR syncing updates, aborting!"; exit 6; }
-rsync -s -av --protect-args --progress "$WORKDIR/bahamut-master/" "$DISTDIR/" || { echo "ERROR syncing updates, aborting!"; exit 7; }
+rsync -s -av --protect-args --progress "$WORKDIR/blyatbeauty-master/" "$DISTDIR/" || { echo "ERROR syncing updates, aborting!"; exit 6; }
