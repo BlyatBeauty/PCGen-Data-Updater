6f5b126c9371c4a942fb2ce93a9311bd2c1a90cf
Misha <66367898+BlyatBeauty@users.noreply.github.com>
2023-01-05 15:42:54 -0500

    MicroRepo Update
    This update changes where the Update script is pointing at when looking for new data. Instead of originally using PCGen's master repo + BahamutDragon's PCGen repo, it now points at a new repo I started which takes its contents from PCGen and BahamutDragon automatically on the backend. 
    The end result is a much smaller download, hopefully increasing the speed at which the script runs and decreases the wear on the end-user's SSD/HDD.

--- UpdateData.bat
+++ UpdateData.bat
@@ -1,16 +1,9 @@
-REM ===== PCGen main repo =====
-powershell -Command "(New-Object Net.WebClient).DownloadFile('http://github.com/PCGen/pcgen/archive/refs/heads/master.zip', 'master.zip')"
-powershell -Command "Expand-Archive -Path master.zip -DestinationPath '%~dp0'"
-echo d | xcopy /Y /E "pcgen-master\data" "%~dp0data"
-del "master.zip"
-RMDIR "pcgen-master" /S /Q
-REM ===== Bahamut Sources repo =====
-powershell -Command "(New-Object Net.WebClient).DownloadFile('http://github.com/BahamutDragon/pcgen/archive/refs/heads/master.zip', 'bahamut.zip')"
-powershell -Command "Expand-Archive -Path bahamut.zip -DestinationPath '%~dp0'"
-echo d | xcopy /Y /E "pcgen-master\data" "%~dp0data"
-echo d | xcopy /Y /E "pcgen-master\outputsheets" "%~dp0outputsheets"
-echo d | xcopy /Y /E "pcgen-master\preview" "%~dp0preview"
-echo d | xcopy /Y /E "pcgen-master\system" "%~dp0system"
-del "bahamut.zip"
-RMDIR "pcgen-master" /S /Q
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/BlyatBeauty/PCGen-Data/archive/refs/heads/main.zip', 'PCGen-Data-main.zip')"
+powershell -Command "Expand-Archive -Path PCGen-Data-main.zip -DestinationPath '%~dp0'"
+echo d | xcopy /Y /E "PCGen-Data-main\data" "%~dp0data"
+echo d | xcopy /Y /E "PCGen-Data-main\outputsheets" "%~dp0outputsheets"
+echo d | xcopy /Y /E "PCGen-Data-main\preview" "%~dp0preview"
+echo d | xcopy /Y /E "PCGen-Data-main\system" "%~dp0system"
+del "PCGen-Data-main.zip"
+RMDIR "PCGen-Data-main" /S /Q
 exit
