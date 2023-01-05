bba94829a822876aa4088992410fabef9e5c0115
Misha <66367898+BlyatBeauty@users.noreply.github.com>
2023-01-05 15:43:45 -0500

    MicroRepo Update
    This update changes where the Update script is pointing at when looking for new data. Instead of originally using PCGen's master repo + BahamutDragon's PCGen repo, it now points at a new repo I started which takes its contents from PCGen and BahamutDragon automatically on the backend. 
    The end result is a much smaller download, hopefully increasing the speed at which the script runs and decreases the wear on the end-user's SSD/HDD.

--- UpdateData.ps1
+++ UpdateData.ps1
@@ -1,16 +1,9 @@
-$Url1 = "http://github.com/PCGen/pcgen/archive/refs/heads/"
-$Url2 = "http://github.com/BahamutDragon/pcgen/archive/refs/heads/"
+$Url = "http://github.com/BlyatBeauty/PCGen-Data/archive/refs/heads/"
 $LocalPath = ".\"
-$File = "master.zip"
-$Data = ".\pcgen-master\data"
-$Bahamut = ".\pcgen-master\*"
-Invoke-WebRequest ($Url1 + $File) -OutFile ($LocalPath + $File)
+$File = "main.zip"
+$BlyatBeauty = ".\PCGen-Data-main\*"
+Invoke-WebRequest ($Url + $File) -OutFile ($LocalPath + $File)
 Expand-Archive -Path ($LocalPath + $File) -DestinationPath $LocalPath
-Copy-Item -Path $Data -Destination $LocalPath -Recurse -PassThru -Force
-Remove-Item -Path ".\pcgen-master" -Recurse
-Remove-Item -Path ($LocalPath + $File) -Force
-Invoke-WebRequest ($Url2 + $File) -OutFile ($LocalPath + $File)
-Expand-Archive -Path ($LocalPath + $File) -DestinationPath $LocalPath
-Copy-Item -Path $Bahamut -Destination $LocalPath -Exclude *.txt, *.md, .gitattributes, .gitignore -Recurse -PassThru -Force
-Remove-Item -Path ".\pcgen-master" -Recurse
+Copy-Item -Path $BlyatBeauty -Destination $LocalPath -Recurse -PassThru -Force
+Remove-Item -Path ".\PCGen-Data-main" -Recurse
 Remove-Item -Path ($LocalPath + $File) -Force
