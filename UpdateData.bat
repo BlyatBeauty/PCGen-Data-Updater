0fa9cbc7981f95333a7f435689a4baa421c060e6
Misha <66367898+BlyatBeauty@users.noreply.github.com>
2023-01-05 14:42:52 -0500

    Update UpdateData.bat

--- UpdateData.bat
+++ UpdateData.bat
@@ -8,9 +8,9 @@ REM ===== Bahamut Sources repo =====
 powershell -Command "(New-Object Net.WebClient).DownloadFile('http://github.com/BahamutDragon/pcgen/archive/refs/heads/master.zip', 'bahamut.zip')"
 powershell -Command "Expand-Archive -Path bahamut.zip -DestinationPath '%~dp0'"
 echo d | xcopy /Y /E "pcgen-master\data" "%~dp0data"
-echo d | xcopy /Y /E "pcgen-master\outputsheets" "%~dp0data"
-echo d | xcopy /Y /E "pcgen-master\preview" "%~dp0data"
-echo d | xcopy /Y /E "pcgen-master\system" "%~dp0data"
+echo d | xcopy /Y /E "pcgen-master\outputsheets" "%~dp0outputsheets"
+echo d | xcopy /Y /E "pcgen-master\preview" "%~dp0preview"
+echo d | xcopy /Y /E "pcgen-master\system" "%~dp0system"
 del "bahamut.zip"
 RMDIR "pcgen-master" /S /Q
 exit
