1f5860b9b5cac03696398c2f4d145ba39a00088e
Misha <66367898+BlyatBeauty@users.noreply.github.com>
2024-11-03 17:08:38 -0500

    Update UpdateData.bat

--- UpdateData.bat
+++ UpdateData.bat
@@ -1,9 +1,6 @@
 powershell -Command "Invoke-WebRequest -Uri 'https://github.com/BlyatBeauty/PCGen-Data/archive/refs/heads/main.zip' -OutFile '%~dp0PCGen-Data-main.zip'"
 powershell -Command "Expand-Archive -Path PCGen-Data-main.zip -DestinationPath '%~dp0'"
 echo d | xcopy /Y /E "PCGen-Data-main\data" "%~dp0data"
-echo d | xcopy /Y /E "PCGen-Data-main\outputsheets" "%~dp0outputsheets"
-echo d | xcopy /Y /E "PCGen-Data-main\preview" "%~dp0preview"
-echo d | xcopy /Y /E "PCGen-Data-main\system" "%~dp0system"
 del "PCGen-Data-main.zip"
 RMDIR "PCGen-Data-main" /S /Q
 exit
